c2026e80971244f1f213dc1fa3bfdbd9386fc41d
ChangYinShung <changyenshung@gmail.com>
2020-04-10 14:52:40 +0800

    update layout

--- ng-alain/src/lib/coding-management-ng-alain-routing.module.ts
+++ ng-alain/src/lib/coding-management-ng-alain-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DynamicLayoutComponent, AuthGuard, PermissionGuard } from '@abp/ng.core';
-import { LayoutPassportComponent, LayoutDefaultComponent } from '@fs/ng-alain/basic';
 import { CodingsModule } from './codings/codings.module';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'code' },
     {
         path: '',
-        component: LayoutDefaultComponent,
+        component: DynamicLayoutComponent,
         // canActivate: [AuthGuard, PermissionGuard],
         children: [
             {
