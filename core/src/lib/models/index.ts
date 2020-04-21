5bb762f756382a851dce279b529f7fadd1a77f2d
samchen <bcsunrsbh@gmail.com>
2020-04-21 14:02:05 +0800

    update code-setting service

--- core/src/lib/models/index.ts
+++ core/src/lib/models/index.ts
@@ -5,3 +5,5 @@ export * from './codes-get-list-input';
 export * from './codes-create-input';
 export * from './codes-update-input';
 export * from './create-or-update-code-settings-input';
+export * from './post-load-code-settings-input-dto';
+export * from './post-load-code-settings-output';
--- core/src/lib/services/code-setting.service.ts
+++ core/src/lib/services/code-setting.service.ts
@@ -1,7 +1,7 @@
 import { RestService } from '@abp/ng.core';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {CreateOrUpdateCodeSettingsInput} from '../models';
+import {CreateOrUpdateCodeSettingsInput, PostLoadCodeSettingsInputDto, PostLoadCodeSettingsOutputDto} from '../models';
 
 @Injectable({providedIn: 'root'})
 export class CodeSettingService {
@@ -10,7 +10,12 @@ export class CodeSettingService {
  postGetCodeSettingsByCodeIdByCodeIds(body: any): Observable<any[]> {
    return this.restService.request({ url: '/api/CodingManagement/codeSetting/getCodeSettingsByCodeId', method: 'POST', body });
  }
+ postLoadCodeSettingsByByInputs(body: PostLoadCodeSettingsInputDto): Observable<PostLoadCodeSettingsOutputDto[]> {
+  return this.restService.request({ url: '/api/CodingManagement/codeSetting/loadCodeSettingsBy', method: 'POST', body });
+}
  postCreateOrUpdateCodeSettingsByInput(body: CreateOrUpdateCodeSettingsInput): Observable<void> {
    return this.restService.request({ url: '/api/CodingManagement/codeSetting/createOrUpdateCodeSettings', method: 'POST', body });
  }
 }
+
+
