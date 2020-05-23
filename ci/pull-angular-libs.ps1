a0d401a0805a5d52c56ede53f1e3f44f0786a5c5
YinChang <yincahng0626@gmail.com>
2020-05-23 15:18:41 +0800

    fix ci

--- ci/pull-angular-libs.ps1
+++ ci/pull-angular-libs.ps1
@@ -1,3 +1,7 @@
 $env:GIT_REDIRECT_STDERR = '2>&1'
 git subtree pull -P angular/libs/coding-management origin angular/libs/coding-management
+if (-Not $?) {
+    Write-Host ("subtree pull failed ")
+    exit $LASTEXITCODE
+}
 
